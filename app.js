12a5837a7d928a3483e3f9a375443d66a5c95342
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-28 22:57:56 +0330

    fetch data by city name

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const BASE_URL =
-  "https://api.openweathermap.org/data/2.5/weather?q=qom&appid=ef3da80186156fbdefb839e83018fa5e&units=metric";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "ef3da80186156fbdefb839e83018fa5e";
+
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
+const getCurrentWeatherByName = async (city) => {
+  const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const responses = await fetch(url);
+  const json = await responses.json();
+  return json;
+};
+
+const searchHandler = async () => {
+  const cityName = searchInput.value;
+  if (!cityName) {
+    alert("please enter city name");
+  }
+  const currentData = await getCurrentWeatherByName(cityName);
+  console.log(currentData);
+};
+
+searchButton.addEventListener("click", searchHandler);
