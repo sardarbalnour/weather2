9b0f45740c3a14a5e225bfa92412efcf77bd4810
Sardar Balnour <balnour.sardar@gmail.com>
2025-12-30 16:01:19 +0330

    getForecastWeatherByCoordinates

--- app.js
+++ app.js
@@ -37,6 +37,13 @@ const getForecastWeatherByName = async (city) => {
   return json;
 };
 
+const getForecastWeatherByCoordinates = async (lat, lon) => {
+  const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const responses = await fetch(url);
+  const json = await responses.json();
+  return json;
+};
+
 const renderCurrentWeather = (data) => {
   const weatherJSX = `
   <h1>${data.name}, ${data.sys.country}</h1>
@@ -60,7 +67,7 @@ const getWeekDay = (date) => {
 };
 
 const renderForecastWeather = (data) => {
-  console.log(data);
+  forecastContainer.innerHTML = "";
   data = data.list.filter((object) => object.dt_txt.endsWith("12:00:00"));
   data.forEach((i) => {
     const forecastJSX = `
@@ -92,6 +99,11 @@ const positionCallback = async (position) => {
   const { latitude, longitude } = position.coords;
   const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
   renderCurrentWeather(currentData);
+  const forecastData = await getForecastWeatherByCoordinates(
+    latitude,
+    longitude
+  );
+  renderForecastWeather(forecastData);
 };
 
 const errorCallback = (error) => {
